ca7266cd882171e704570738796a08b616391b8f
Tomás Sasovsky <tomassasovsky@Tomas-iMac.local>
2021-05-27 16:48:48 -0300

    Provider - Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 import 'package:state_management/pages/first_page.dart';
 import 'package:state_management/pages/second_page.dart';
+import 'package:state_management/services/user_service.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: 'first',
-      routes: {
-        'first': (_) => FirstPage(),
-        'second': (_) => SecondPage(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserService()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: 'first',
+        routes: {
+          'first': (_) => FirstPage(),
+          'second': (_) => SecondPage(),
+        },
+      ),
     );
   }
 }
--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management/models/user.dart';
+import 'package:state_management/services/user_service.dart';
 
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final userService = Provider.of<UserService>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Page'),
+        title: userService.userExists ? Text('${userService.user.name}') : Text('Second Page'),
       ),
       body: Center(
         child: Column(
@@ -14,17 +18,24 @@ class SecondPage extends StatelessWidget {
             MaterialButton(
               child: Text('Set User', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
-              onPressed: () {},
+              onPressed: () {
+                final user = User(name: 'Tomás', professions: ['Full Stack Dev', 'Flutter Dev']);
+                Provider.of<UserService>(context, listen: false).loadUser = user;
+              },
             ),
             MaterialButton(
               child: Text('Change Age', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<UserService>(context, listen: false).userAge = 20;
+              },
             ),
             MaterialButton(
               child: Text('Add Profession', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<UserService>(context, listen: false).addProfession();
+              },
             ),
           ],
         ),
